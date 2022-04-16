509a0bbd82cf075c315f697bdfe48caa0d3bcdda
RomanJelinek <roman.jelinek91@gmail.com>
2022-04-16 09:47:28 +0200

    added a lookout place and updated photos

--- src/spotsData.ts
+++ src/spotsData.ts
@@ -35,9 +35,14 @@ const spotsData = [
     text: 'Na tomto místě se nachází obnovená skalní kaplička Panny Marie Lurdské. Vysvěcena byla píseckým vikářem Karlem Vrbou v roce 2012. Původní kaplička pocházela z roku 1871. Tlučkův mlýn stávající nedaleko od těchto míst je poprvé zmiňován již kolem roku 1405 coby majetek špitálu u Kamenného mostu v Písku, založeného rytířským řádem Křížovníků s červenou hvězdou. Roku 1860 koupil mlýn borečnický statkář Jan Nosek pro svoji dceru Marii. Ta se s bohatým věnem provdala za Aloise Kubrichta.Na žádost své babičky nechal mlynář Kubrichta vylámat ve skále nad mlýnem jeskyni dle vzoru Lurd-známého poutního místa v Pyrenejích. V takto vzniklé kapličce byla umístěna socha Panny Marie Lurdské a 1.května 1871 slavnostně vysvěcena.',
     pictures: [
       {
-        src: 'https://lh5.googleusercontent.com/p/AF1QipN9xuNjsSCCbCY0CR1XW6k_GUJELecyz8nQ8BfU=s901-k-no',
-        width: 6,
-        height: 4,
+        src: 'https://d34-a.sdn.cz/d_34/c_img_QQ_k/6UCHhP.jpeg',
+        width: 7,
+        height: 5,
+      },
+      {
+        src: 'https://d34-a.sdn.cz/d_34/c_img_gW_Y/6xlDWc.jpeg',
+        width: 1,
+        height: 1,
       },
       {
         src: 'https://d34-a.sdn.cz/d_34/c_A_C/SEdzKi.jpeg',
@@ -45,24 +50,50 @@ const spotsData = [
         height: 3,
       },
       {
-        src: 'https://lh5.googleusercontent.com/p/AF1QipPCthEgrT3sLo0G444C9DjCpE2esQPDx7liIF5J=s773-k-no',
-        width: 5,
-        height: 4,
-      },
-      {
-        src: 'https://lh5.googleusercontent.com/p/AF1QipMzUazxE4ZlU-rxUi3XxveGNqP348zKCBY2BKgj=s1016-k-no',
+        src: 'https://d34-a.sdn.cz/d_34/c_img_QJ_V/GopDk.jpeg',
         width: 3,
         height: 4,
       },
       {
-        src: 'https://lh5.googleusercontent.com/p/AF1QipMYnTkz-VCmnFVs8KSgid0A_ERW5pBLcljXpfXo=s1016-k-no',
+        src: 'https://d34-a.sdn.cz/d_34/c_img_QK_X/FjeNB.jpeg',
         width: 3,
         height: 4,
       },
       {
-        src: 'https://lh5.googleusercontent.com/p/AF1QipP34_nLrUBvYIJ5r6kMDLFrAC9B9PvyqrcmpVbV=s1354-k-no',
-        width: 3,
+        src: 'https://d34-a.sdn.cz/d_34/c_img_QL_n/hzdFRu.jpeg',
+        width: 6,
         height: 5,
+      },
+    ],
+  },
+  {
+    id: 'vyhlidka',
+    top: '-8vw',
+    left: '53vw',
+    topMobile: '-3%',
+    leftMobile: '47%',
+    headline: 'Vyhlídkové místo',
+    text: 'Krásná vyhlídka na řeku Otavu',
+    pictures: [
+      {
+        src: 'https://d34-a.sdn.cz/d_34/c_B_C/kNWTN7.jpeg',
+        width: 3,
+        height: 4,
+      },
+      {
+        src: 'https://d34-a.sdn.cz/d_34/c_img_gW_N/NvHhB5.jpeg',
+        width: 5,
+        height: 3,
+      },
+      {
+        src: 'https://d34-a.sdn.cz/d_34/c_img_QN_c/a42vN.jpeg',
+        width: 2,
+        height: 3,
+      },
+      {
+        src: 'https://d34-a.sdn.cz/d_34/c_img_F_J/AtCEbQ.jpeg',
+        width: 4,
+        height: 3,
       },
     ],
   },
